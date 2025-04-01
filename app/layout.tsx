df8406032f114ddd4f590d9988d4c48bd5fce13d
molecule-lab-rushil <rushildhinoja@moleculelab.in>
2025-04-01 17:10:00 +0530

    Add simple analytics also

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import type { Metadata } from "next";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
     <html lang='en'>
       <body className={inter.className}>{children}</body>
       <GoogleAnalytics gaId='G-MSP27XE5GZ' />
+      <Script src='https://scripts.simpleanalyticscdn.com/latest.js' />
     </html>
   );
 }
